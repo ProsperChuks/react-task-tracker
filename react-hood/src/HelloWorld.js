ca61ca2337999afa45873e2f2d1b0a9f6c3cb44c
i-am-prinx <geek_200@outlook.com>
2018-08-04 22:26:05 +0100

    initial commit

---
+++ react-hood/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div className="hello-world" >
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
+};
+
+export default HelloWorld;
