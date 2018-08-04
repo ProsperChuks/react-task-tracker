8faad6e5752db8caa394a3a59977bc3bbca9466b
i-am-prinx <geek_200@outlook.com>
2018-08-04 23:07:47 +0100

    Hello World

--- react-hood/src/HelloWorld.js
+++ react-hood/src/HelloWorld.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './HelloWorld.css'
 
 
 class HelloWorld extends Component {
     render() {
         return (
             <div className="hello-world" >
-                <h1>Hello World!</h1>
+                <h1>Hello, World!</h1>
             </div>
         )
     }
