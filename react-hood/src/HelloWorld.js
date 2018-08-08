a51825eea64845700a21f25fd6c958f89c4b3f92
i-am-prinx <geek_200@outlook.com>
2018-08-08 09:58:43 +0100

    kanbanboard project ( commented out Hello World )

--- react-hood/src/HelloWorld.js
+++ react-hood/src/HelloWorld.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import './HelloWorld.css'
+// import React, { Component } from 'react';
+// import './HelloWorld.css'
 
 
-class HelloWorld extends Component {
-    render() {
-        return (
-            <div className="hello-world" >
-                <h1>{this.props.greeting} {this.props.object}!</h1>
-            </div>
-        )
-    }
-};
+// class HelloWorld extends Component {
+//     render() {
+//         return (
+//             <div className="hello-world" >
+//                 <h1>{this.props.greeting} {this.props.object}!</h1>
+//             </div>
+//         )
+//     }
+// };
 
-export default HelloWorld;
+// export default HelloWorld;
--- react-hood/src/index.js
+++ react-hood/src/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './HelloWorld';
+import KanbanBoard from './components/KanbanBoard';
 import './index.css';
 
+let cardsList = [
+    {
+        id: 1,
+        title: 'Read the Book',
+        description: 'I should read the whole book',
+        status: 'in-progress',
+        tasks: []
+    },
+
+    {
+        id: 2,
+        title: 'Write some code',
+        description: 'Code along with the samples in the book',
+        status: 'todo',
+        tasks: [
+            {
+                id: 1,
+                name: 'ContactList Example',
+                done: true
+            },
+
+            {
+                id: 2,
+                name: 'kanban Example',
+                done: false
+            },
+
+            {
+                id: 3,
+                name: 'My own experiments',
+                done: false
+            }
+        ]
+    }
+];
+
 ReactDOM.render(
-    < HelloWorld greeting="GoodMorning" object="Prince" />,
-    document.getElementById('root')
+    <KanbanBoard cards={cardsList} />, document.getElementById("root")
 );
