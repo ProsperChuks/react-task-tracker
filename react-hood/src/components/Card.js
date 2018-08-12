a57a2875cebf861f3f749f52d88162f3f706a71f
i-am-prinx <geek_200@outlook.com>
2018-08-12 23:41:12 +0100

    efficient toggleEvent, ternary operator 4 clsName

--- react-hood/src/components/Card.js
+++ react-hood/src/components/Card.js
@@ -8,6 +8,12 @@ class Card extends Component {
             showDetails: false
         }
     }
+
+    toggleDetails() {
+        this.setState({ showDetails: !this.state.showDetails });
+    }
+
+
     render() {
         let cardDetails;
         if (this.state.showDetails) {
@@ -20,11 +26,13 @@ class Card extends Component {
             );
 
         };
+
         return (
             <div className="card">
-                <div className="card__title" onClick={
-                    () => this.setState({ showDetails: !this.state.showDetails })} >
-                    {this.props.title}
+                <div className={this.state.showDetails ?
+                    "card__title card__title--is-open" : "card__title"
+                } onClick={
+                    this.toggleDetails.bind(this)} > {this.props.title}
                 </div>
                 {cardDetails}
             </div>
