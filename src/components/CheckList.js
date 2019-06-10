a91562d5b81104b12c465c1263716e8bf9f090df
iamprinx <geek_200@outlook.com>
2019-06-10 21:36:18 +0100

    fix warning, provide key to list item

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class CheckList extends Component {
   render() {
     let tasks = this.props.tasks.map(task => (
-      <li className="checklist__task">
+      <li key={task.id} className="checklist__task">
         <input type="checkbox" defaultChecked={task.done} />
         {task.name}
         <a href="#" className="checklist__task--remove" />
