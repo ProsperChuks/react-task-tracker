c6b82b8aee985b8dbec81a65455a7d712350a4ce
iamprinx <geek_200@outlook.com>
2019-06-10 20:45:35 +0100

    include input field to insert a task to checklist with styling

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CheckList extends Component {
-    render() {
-        let tasks = this.props.tasks.map((task) => (
-            <li className="checklist__task">
-                <input type="checkbox" defaultChecked={task.done} />
-                {task.name}
-                <a href="#" className="checklist__task--remove" />
-            </li>
-        ));
+  render() {
+    let tasks = this.props.tasks.map(task => (
+      <li className="checklist__task">
+        <input type="checkbox" defaultChecked={task.done} />
+        {task.name}
+        <a href="#" className="checklist__task--remove" />
+      </li>
+    ));
 
-        return (
-            <div className="checklist">
-                <ul> {tasks} </ul>
-            </div>
-        );
-    }
+    return (
+      <div className="checklist">
+        <ul> {tasks} </ul>
+        <input
+          type="text"
+          className="checklist--add-task"
+          placeholder="Type then hit enter to add a task"
+        />
+      </div>
+    );
+  }
 }
 
 export default CheckList;
