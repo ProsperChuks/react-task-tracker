f4e8f2efc2225fc9dbef01020c05ccc304c8b13e
iamprinx <geek_200@outlook.com>
2019-06-10 01:38:50 +0100

    include inline style to give card sidebar distinct color

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
 class List extends Component {
-    render() {
-        var cards = this.props.cards.map((card) => {
-            return <Card
-                id={card.id}
-                title={card.title}
-                description={card.description}
-                tasks={card.tasks} />
-        });
+  render() {
+    var cards = this.props.cards.map(card => {
+      return (
+        <Card
+          id={card.id}
+          title={card.title}
+          color={card.color}
+          description={card.description}
+          tasks={card.tasks}
+        />
+      );
+    });
 
-        return (
-            <div className="list" >
-                <h1> {this.props.title} </h1>
-                {cards}
-            </div>
-        );
-    }
+    return (
+      <div className="list">
+        <h1> {this.props.title} </h1>
+        {cards}
+      </div>
+    );
+  }
 }
 
 export default List;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import KanbanBoard from './components/KanbanBoard';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import KanbanBoard from "./components/KanbanBoard";
+import "./index.css";
 
 let cardsList = [
-    {
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the **whole** book",
+    color: "#bd8d31",
+    status: "in-progress",
+    tasks: []
+  },
+
+  {
+    id: 2,
+    title: "Write some code",
+    description:
+      "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
+    color: "#3a7e28",
+    status: "todo",
+    tasks: [
+      {
         id: 1,
-        title: 'Read the Book',
-        description: 'I should read the **whole** book',
-        status: 'in-progress',
-        tasks: []
-    },
+        name: "ContactList Example",
+        done: true
+      },
 
-    {
+      {
         id: 2,
-        title: 'Write some code',
-        description: 'Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)',
-        status: 'todo',
-        tasks: [
-            {
-                id: 1,
-                name: 'ContactList Example',
-                done: true
-            },
+        name: "kanban Example",
+        done: false
+      },
 
-            {
-                id: 2,
-                name: 'kanban Example',
-                done: false
-            },
-
-            {
-                id: 3,
-                name: 'My own experiments',
-                done: false
-            }
-        ]
-    }
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  }
 ];
 
 ReactDOM.render(
-    <KanbanBoard cards={cardsList} />, document.getElementById("root")
+  <KanbanBoard cards={cardsList} />,
+  document.getElementById("root")
 );
