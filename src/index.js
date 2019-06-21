bf94543d54b0bd8ebdcb329dc7577fb5318f2edb
iamprinx <geek_200@outlook.com>
2019-06-21 22:08:19 +0100

    render container component

--- src/index.js
+++ src/index.js
@@ -1,49 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import KanbanBoard from "./components/KanbanBoard";
+import KanbanBoardContainer from "./components/KanbanBoardContainer";
 import "./index.css";
 
-let cardsList = [
-  {
-    id: 1,
-    title:
-      "This is a new card with a very very long title just to test our custom proptype defined for a card title",
-    description: "I should read the **whole** book",
-    color: "#bd8d31",
-    status: "in-progress",
-    tasks: []
-  },
+// let cardsList = [
+//   {
+//     id: 1,
+//     title: "Read the Book",
+//     description: "I should read the **whole** book",
+//     color: "#bd8d31",
+//     status: "in-progress",
+//     tasks: []
+//   },
 
-  {
-    id: 2,
-    title: "Write some code",
-    description:
-      "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
-    color: "#3a7e28",
-    status: "todo",
-    tasks: [
-      {
-        id: 1,
-        name: "ContactList Example",
-        done: true
-      },
+//   {
+//     id: 2,
+//     title: "Write some code",
+//     description:
+//       "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
+//     color: "#3a7e28",
+//     status: "todo",
+//     tasks: [
+//       {
+//         id: 1,
+//         name: "ContactList Example",
+//         done: true
+//       },
 
-      {
-        id: 2,
-        name: "kanban Example",
-        done: false
-      },
+//       {
+//         id: 2,
+//         name: "kanban Example",
+//         done: false
+//       },
 
-      {
-        id: 3,
-        name: "My own experiments",
-        done: false
-      }
-    ]
-  }
-];
+//       {
+//         id: 3,
+//         name: "My own experiments",
+//         done: false
+//       }
+//     ]
+//   }
+// ];
 
-ReactDOM.render(
-  <KanbanBoard cards={cardsList} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<KanbanBoardContainer />, document.getElementById("root"));
