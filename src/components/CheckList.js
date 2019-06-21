e0bb5e262952ab8e32788eb5f547f326b10d3a78
iamprinx <geek_200@outlook.com>
2019-06-21 23:30:46 +0100

    receive task CRUD prototype & apply appropriately

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -2,12 +2,30 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class CheckList extends Component {
+
+  checkInputKeyPress(evt) {
+    if (evt.key === "Enter") {
+      this.props.taskCallbacks.add(this.props.cardId, evt.target.value);
+      evt.target.value = '';
+    }
+  }
+
+
   render() {
-    let tasks = this.props.tasks.map(task => (
+    let tasks = this.props.tasks.map((task, taskIndex) => (
       <li key={task.id} className="checklist__task">
-        <input type="checkbox" defaultChecked={task.done} />
+        <input
+          type="checkbox"
+          defaultChecked={task.done}
+          onChange={
+            this.props.taskCallbacks.toggle.bind(
+              null, this.props.CardId, task.id, taskIndex
+            )
+          } />
         {task.name}
-        <a href="#" className="checklist__task--remove" />
+        <a href="#" className="checklist__task--remove" onClick={
+          this.props.taskCallbacks.delete(null, this.props.cardId, task.id, taskIndex)
+        } />
       </li>
     ));
 
@@ -18,6 +36,7 @@ class CheckList extends Component {
           type="text"
           className="checklist--add-task"
           placeholder="Type then hit enter to add a task"
+          onKeyPress={this.checkInputKeyPress.bind(this)}
         />
       </div>
     );
@@ -26,6 +45,7 @@ class CheckList extends Component {
 
 CheckList.propTypes = {
   cardId: PropTypes.number,
+  taskCallbacks: PropTypes.object,
   tasks: PropTypes.arrayOf(PropTypes.object)
 };
 export default CheckList;
